616fbf1e2dd0003ee0de8bb51a58171c6d1a8785
thong-vm <thongvmsyncpartners@gmail.com>
2024-09-17 15:40:58 +0900

    update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import ModelInference from "./ModelInference";
 const App = () => {
   return (
     <div className="w-full h-full bg-[#467D3F]">
-      {/* <div className="flex justify-center">
-        <span className="text-xl font-bold text-white"></span>
-      </div> */}
       <ModelInference />
     </div>
   );
